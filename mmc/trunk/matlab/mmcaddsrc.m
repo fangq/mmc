b7295ecb590b8c813bff92b0c10d40893fda5df8
Qianqian Fang <fangqq@gmail.com>
2015-04-18 13:31:35 -0400

    a generalized mmcaddsrc to handel various source types

--- mmc/trunk/matlab/mmcaddsrc.m
+++ mmc/trunk/matlab/mmcaddsrc.m
@@ -12,6 +12,13 @@ function [newnode,newelem]=mmcaddsrc(node,elem,src,varargin)
 % input:
 %     node: the node list of the original mesh
 %     elem: the elem list of the original mesh
+%     src:  the coordinates of the vertices that define the source domain
+%           if src is a struct, it will be treated as an MMCLAB input
+%           structure and the source vertices will be computed
+%           automatically
+%     opt: optional, a struct defining additional options, the possible fields
+%              are identical to those supported by meshrefine.m in iso2mesh.
+%              the opt struct can be replaced by a list of 'param','value' pairs
 %
 % output:
 %     newnode: the node list of the modified mesh, by default, 
@@ -20,9 +27,7 @@ function [newnode,newelem]=mmcaddsrc(node,elem,src,varargin)
 %              between the convex hull of the combined node/src and
 %              the original mesh surface are labeled as 0; the elements
 %              sharing the source domain are labeled as -1.
-%     opt: optional, a struct defining additional options, the possible fields
-%              are identical to those supported by meshrefine.m in iso2mesh.
-%              the opt struct can be replaced by a list of 'param','value' pairs
+%
 % example:
 %     [node,face,elem]=meshasphere([0 0 0],24,1,2);
 %     elem(:,5)=1;
@@ -35,6 +40,21 @@ function [newnode,newelem]=mmcaddsrc(node,elem,src,varargin)
 %     % example with additional options
 %     [newnode,newelem]=mmcaddsrc(node,elem,source,'extcmdopt','-Y -a1','extlabel',-2);
 %
+%     % example with mmclab cfg input
+%     cfg=struct('srctype','cone','srcpos',[0 0 28],'srcdir',[0 0 -1]);
+%     [newnode,newelem]=mmcaddsrc(node,elem,cfg);
+%     figure; plotmesh(newnode,newelem);
+%
+%     % for box-shaped objects, you have to use meshabox with matlab 7.11
+%     % or newer, older matlab versions will fail to get the convex hull
+%     [node,face,elem]=meshabox([-10 -10 -10],[10 10 0],1,2);
+%     elem(:,5)=1;
+%
+%     cfg=struct('srctype','fourier','srcpos',[-5 -5 10],'srcdir',...
+%        [0 0 -1],'srcparam1',[10 0 0 3],'srcparam2',[0 8 0 1]);
+%     [newnode,newelem]=mmcaddsrc(node,elem,cfg);
+%     figure; plotmesh(newnode,newelem);
+%
 % this file is part of Mesh-based Monte Carlo (MMC)
 %
 % License: GPLv3, see http://mcx.sf.net/mmc/ for details
@@ -43,6 +63,9 @@ function [newnode,newelem]=mmcaddsrc(node,elem,src,varargin)
 opt=varargin2struct(varargin{:});
 if(~isfield(opt,'extcmdopt'))
    opt.extcmdopt='-Y';
+end
+if(isstruct(src))
+    src=mmcsrcdomain(src,[min(node);max(node)]);
 end
 opt.newnode=src;
 [newnode,newelem]=meshrefine(node,elem,opt);
---
+++ mmc/trunk/matlab/mmcsrcdomain.m
@@ -0,0 +1,81 @@
+function [srcnode,srcface]=mmcsrcdomain(cfg,meshbbx,varargin)
+%
+% [srcnode,srcface]=mmcsrcdomain(cfg)
+%
+% Defining a source domain (for launching new photons) in the form of 
+% polyhedra based on an MMCLAB simulation configuration structure
+%
+% author: Qianqian Fang (fangq <at> nmr.mgh.harvard.edu)
+%
+% input:
+%     cfg: the simulation configuration structure used by mmclab, the
+%          following fields are required
+%               cfg.srcpos, cfg.srcdir, cfg.srctype, cfg.srcparam1,
+%               cfg.srcparam2
+%          run "help mmclab" for details.
+%     meshbbx: the bounding box of the original mesh in the form of
+%          [min(node); max(node)], i.e. a 2x3 matrix
+%
+% output:
+%     srcnode: the vertices of the source domain
+%     srcface (optional): the polyhedral patches of the source domain
+%
+% example:
+%     [node,face,elem]=meshasphere([0 0 0],20,1);
+%     elem(:,5)=1;
+%     cfg=struct('srctype','cone','srcpos',[0 0 25],'srcdir',[0 0 -1]);
+%     srccone=mmcsrcdomain(cfg,[min(node);max(node)]);
+%
+%     cfg=struct('srctype','fourier','srcpos',[-5 -5 25],'srcdir',...
+%        [0 0 -1],'srcparam1',[10 0 0 3],'srcparam2',[0 8 0 1]);
+%     srcfourier=mmcsrcdomain(cfg,[min(node);max(node)]);
+%
+% this file is part of Mesh-based Monte Carlo (MMC)
+%
+% License: GPLv3, see http://mcx.sf.net/mmc/ for details
+%
+
+% the launching domain should be slightly larger than the actual source
+% domain to avoid starting from an edge or vertex
+expandsion=1.1; 
+
+%opt=varargin2struct(varargin{:});
+if(~isstruct(cfg))
+   error('input cfg must be a struct');
+end
+if(~isfield(cfg,'srcpos') || ~isfield(cfg,'srcdir') || ...
+   ~isfield(cfg,'srctype') )
+    error('you must at least define cfg.srcpos, cfg.srcdir, cfg.srctype');
+end
+
+if(strcmp(cfg.srctype,'isotropic'))
+    cfg.srcdir=rand(1,3);
+    cfg.srcdir=cfg.srcdir/norm(cfg.srcdir);
+end
+cfg.srcdir=cfg.srcdir/norm(cfg.srcdir);
+domainradius=min(meshbbx(2,:)-meshbbx(1,:))*0.25;
+if(strcmp(cfg.srctype,'pencil'))
+   warning(['for external pencil beams, you are highly recommended to set ' ...
+       'cfg.e0=''>'', instead of using this script']); 
+end
+
+if(strcmp(cfg.srctype,'pencil') || strcmp(cfg.srctype,'cone') || ...
+        strcmp(cfg.srctype,'zgaussian')|| strcmp(cfg.srctype,'isotropic') || strcmp(cfg.srctype,'arcsine'))
+    srcnode=orthdisk(cfg.srcpos,cfg.srcpos+cfg.srcdir,domainradius,3);
+    srcface=[1 2 3];
+elseif(strcmp(cfg.srctype,'planar') || strcmp(cfg.srctype,'pattern') || ...
+        strcmp(cfg.srctype,'fourier') ||strcmp(cfg.srctype,'fourierx')||strcmp(cfg.srctype,'fourier2d'))
+    v0=cfg.srcpos(1:3);
+    v1=cfg.srcparam1(1:3);
+    v2=cfg.srcparam2(1:3);
+    if(strcmp(cfg.srctype,'fourierx')||strcmp(cfg.srctype,'fourier2d'))
+        v2=cross(v0,v1);
+    end
+    srcnode=[v0; v0+v1; v0+v1+v2; v0+v2];
+    srcface=[1 2 3;3 4 1];
+elseif(strcmp(cfg.srctype,'disk'))
+    srcnode=orthdisk(cfg.srcpos,cfg.srcpos+cfg.srcdir,cfg.srcparam1(1)*expandsion,100);
+    srcface=delaunay(srcnode(:,1),srcnode(:,2));
+else
+    error(['source type not supported: ', cfg.srctype]);
+end
