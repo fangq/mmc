a797230490ce3cbbf77bb4978c9706df9b8189b7
Qianqian Fang <fangqq@gmail.com>
2017-06-06 00:42:58 -0400

    support older version of MATLAB

--- mmc/trunk/commons/Makefile_common.mk
+++ mmc/trunk/commons/Makefile_common.mk
@@ -43,7 +43,7 @@ ifeq ($(findstring x86_64,$(ARCH)), x86_64)
 endif
 
 MKMEX      :=mex
-MKMEXOPT    =CC='$(CC)' CXX='$(CXX)' CXXFLAGS='$(CCFLAGS) $(USERCCFLAGS)' LDFLAGS='-L$$MATLABROOT/sys/os/$$ARCH $$LDFLAGS $(MEXLINKOPT)' $(FASTMATH) -cxx -outdir $(BINDIR) -liomp5
+MKMEXOPT    =CC='$(CC)' CXX='$(CXX)' CXXFLAGS='$(CCFLAGS) $(USERCCFLAGS)' LDFLAGS='-L$$TMW_ROOT$$MATLABROOT/sys/os/$$ARCH $$LDFLAGS $(MEXLINKOPT)' $(FASTMATH) -cxx -outdir $(BINDIR) -liomp5
 MKOCT      :=mkoctfile
 
 DLLFLAG=-fPIC
