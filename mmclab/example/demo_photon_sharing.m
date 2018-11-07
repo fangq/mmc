c6cfdca3b575b0c671fec02a6da04d869608e614
Ruoyang Yao <yaoruoyang@gmail.com>
2018-11-06 20:54:33 -0500

    complete photon sharing example for mmclab

--- mmclab/example/demo_photon_sharing.m
+++ mmclab/example/demo_photon_sharing.m
@@ -4,7 +4,7 @@
 
 %% prepare simulation input
 
-cfg.nphoton=1e5;
+cfg.nphoton=3e6;
 [cfg.node face cfg.elem]=meshabox([0 0 0],[60 60 20],2, 2);
 cfg.elemprop=ones(size(cfg.elem,1),1);
 cfg.srcpos=[10 10 -2];
@@ -33,37 +33,41 @@ cfg.srcpattern = pat;
 %% run the simulation
 
 flux=mmclab(cfg);
-% 
-% %% plot results
-% 
-% figure();
-% 
-% subplot(1,3,1); title('pattern 1');
-% plotmesh([node,(cwdata(1,:))'],elem,'linestyle','none');
-% shading interp
-% axis off
-% view([0,0,-1])
-% 
-% subplot(2,3,2); title('pattern 2');
-% plotmesh([node,(cwdata(2,:))'],elem,'linestyle','none');
-% shading interp
-% axis off
-% view([0,0,-1])
-% 
-% subplot(2,3,3); title('pattern 3');
-% plotmesh([node,(cwdata(3,:))'],elem,'linestyle','none');
-% shading interp
-% axis off
-% view([0,0,-1])
-% 
-% subplot(2,3,5); title('pattern 4');
-% plotmesh([node,(cwdata(4,:))'],elem,'linestyle','none');
-% shading interp
-% axis off
-% view([0,0,-1])
-% 
-% subplot(2,3,6); title('pattern 5');
-% plotmesh([node,(cwdata(5,:))'],elem,'linestyle','none');
-% shading interp
-% axis off
-% view([0,0,-1])
+
+%% plot results (same as in the mmc example)
+
+[node, ~, elem]=meshabox([0 0 0],[60 60 20],2, 2);
+data = flux.data(:,1:length(node),:);
+cwdata = squeeze(sum(data,3));
+
+figure();
+
+subplot(1,3,1); title('pattern 1');
+plotmesh([node,(cwdata(1,:))'],elem,'linestyle','none');
+shading interp
+axis off
+view([0,0,-1])
+
+subplot(2,3,2); title('pattern 2');
+plotmesh([node,(cwdata(2,:))'],elem,'linestyle','none');
+shading interp
+axis off
+view([0,0,-1])
+
+subplot(2,3,3); title('pattern 3');
+plotmesh([node,(cwdata(3,:))'],elem,'linestyle','none');
+shading interp
+axis off
+view([0,0,-1])
+
+subplot(2,3,5); title('pattern 4');
+plotmesh([node,(cwdata(4,:))'],elem,'linestyle','none');
+shading interp
+axis off
+view([0,0,-1])
+
+subplot(2,3,6); title('pattern 5');
+plotmesh([node,(cwdata(5,:))'],elem,'linestyle','none');
+shading interp
+axis off
+view([0,0,-1])
