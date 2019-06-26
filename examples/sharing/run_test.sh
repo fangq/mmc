d81fd9a21891b5e95a22aa0dafefd5186200f889
Qianqian Fang <fangqq@gmail.com>
2019-06-26 13:03:28 -0400

    dualmode mmc - support both SSE4+cpu (-G -1) and CPU, rename to mmcl

--- examples/sharing/run_test.sh
+++ examples/sharing/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../../src/bin/mmc -n 1000000 -f sharing.inp -s sharing -b 1 -k 0 -D TP
+../../src/bin/mmcl -n 1000000 -f sharing.inp -s sharing -b 1 -k 0 -D TP
--- examples/validation/run_test.sh
+++ examples/validation/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-time ../../src/bin/mmcx -f cube.inp -s cube -n 30000000 -b 0 -D TP $@
+time ../../src/bin/mmcl -f cube.inp -s cube -n 30000000 -b 0 -D TP $@
