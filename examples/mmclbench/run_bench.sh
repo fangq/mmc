d2f193fdbbbcd7d8ce8cb9f13a8abf0e9dca1b8e
Qianqian Fang <fangqq@gmail.com>
2019-06-27 23:21:06 -0400

    update script

--- examples/mmclbench/run_bench.sh
+++ examples/mmclbench/run_bench.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+OPT="-F bin -n 1e6 -D T -l"
+EXTRA=${@:2}
+CPUEXTRA="-n 1e7"
+
+echo OPT=$OPT
+echo EXTRA=$EXTRA
+
 ################################################################
 # B1
 ################################################################
@@ -9,16 +16,16 @@ then
     cd ../validation
 
     echo testing benchmark B1 mmc mode with mmc
-    ./run_test.sh -s b1_mmc -F bin -n 1e6 -G -1 -M S -D T -l $@
+    ./run_test.sh -s b1_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA
 
     echo testing benchmark B1 mmc mode with mmcl
-    ./run_test.sh -s b1_mmcl -F bin -n 1e6 -G 1 -M S -D T -l $@
+    ./run_test.sh -s b1_mmcl $OPT -G 1 -M S $EXTRA
 
     echo testing benchmark B1 dmmc mode with mmc
-    ./run_test.sh -s b1_dmmc -F bin -n 1e6 -G -1 -M G -D T -l $@
+    ./run_tess.sh -s b1_dmmc $OPT -G -1 -M G $EXTRA $CPUEXTRA
 
     echo testing benchmark B1 dmmc mode with mmcl
-    ./run_test.sh -s b1_dmmcl -F bin -n 1e6 -G 1 -M G -D T -l $@
+    ./run_tess.sh -s b1_dmmcl $OPT -G 1 -M G $EXTRA
 
     cp b1_*mmc*.log ../mmclbench
 
@@ -33,16 +40,16 @@ then
     cd ../sphshells
 
     echo testing benchmark B2 mmc mode with mmc
-    ./run_mmc.sh -s b2_mmc -F bin -n 1e6 -G -1 -M S -D T -l $@
+    ./run_mmc.sh -s b2_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA
 
     echo testing benchmark B2 mmc mode with mmcl
-    ./run_mmc.sh -s b2_mmcl -F bin -n 1e6 -G 1 -M S -D T -l $@
+    ./run_mmc.sh -s b2_mmcl $OPT -G 1 -M S $EXTRA
 
     echo testing benchmark B2 dmmc mode with mmc
-    ./run_dmmc.sh -s b2_dmmc -F bin -n 1e6 -G -1 -M G -D T -l $@
+    ./run_dmmc.sh -s b2_dmmc $OPT -G -1 -M G $EXTRA $CPUEXTRA
 
     echo testing benchmark B2 dmmc mode with mmcl
-    ./run_dmmc.sh -s b2_dmmcl -F bin -n 1e6 -G 1 -M G -D T -l $@
+    ./run_dmmc.sh -s b2_dmmcl $OPT -G 1 -M G $EXTRA
 
     cp b2_*mmc*.log ../mmclbench
 fi
@@ -57,16 +64,16 @@ then
     cd ../colin27
 
     echo testing benchmark B3 mmc mode with mmc
-    ./run_mmc.sh -s b3_mmc -F bin -n 1e6 -G -1 -M S -D T -l $@
+    ./run_mmc.sh -s b3_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA
 
     echo testing benchmark B3 mmc mode with mmcl
-    ./run_mmc.sh -s b3_mmcl -F bin -n 1e6 -G 1 -M S -D T -l $@
+    ./run_mmc.sh -s b3_mmcl $OPT -G 1 -M S $EXTRA
 
     echo testing benchmark B3 dmmc mode with mmc
-    ./run_dmmc.sh -s b3_dmmc -F bin -n 1e6 -G -1 -M G -D T -l $@
+    ./run_dmmc.sh -s b3_dmmc $OPT -G -1 -M G $EXTRA $CPUEXTRA
 
     #echo testing benchmark B3 dmmc mode with mmcl
-    #./run_dmmc.sh -s b3_dmmcl -F bin -n 1e6 -G 1 -M G -D T -l $@
+    #./run_dmmc.sh -s b3_dmmcl $OPT -G 1 -M G $EXTRA
 
     cp b3_*mmc*.log ../mmclbench
 fi
@@ -80,16 +87,16 @@ then
     cd ../skinvessel
 
     echo testing benchmark B4 mmc mode with mmc
-    ./run_mmc.sh -s b4_mmc -F bin -n 1e6 -G -1 -M S -D T -l $@
+    ./run_mmc.sh -s b4_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA
 
     echo testing benchmark B4 mmc mode with mmcl
-    ./run_mmc.sh -s b4_mmcl -F bin -n 1e6 -G 1 -M S -D T -l $@
+    ./run_mmc.sh -s b4_mmcl $OPT -G 1 -M S $EXTRA
 
     echo testing benchmark B4 dmmc mode with mmc
-    ./run_dmmc.sh -s b4_dmmc -F bin -n 1e6 -G -1 -M G -D T -l $@
+    ./run_dmmc.sh -s b4_dmmc $OPT -G -1 -M G $EXTRA $CPUEXTRA
 
     echo testing benchmark B4 dmmc mode with mmcl
-    ./run_dmmc.sh -s b4_dmmcl -F bin -n 1e6 -G 1 -M G -D T -l $@
+    ./run_dmmc.sh -s b4_dmmcl $OPT -G 1 -M G $EXTRA
 
     cp b4_*mmc*.log ../mmclbench
 fi
