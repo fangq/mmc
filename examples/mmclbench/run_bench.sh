e53702194baf6e149d460397acba45eeefa735bd
Qianqian Fang <fangqq@gmail.com>
2019-06-28 22:56:30 -0400

    further update benchmark script

--- examples/mmclbench/run_bench.sh
+++ examples/mmclbench/run_bench.sh
@@ -5,7 +5,7 @@
 
 OPT="-F bin -n 1e6 -D T"
 EXTRA=${@:2}
-CPUEXTRA="-n 1e7"
+CPUEXTRA="-n 1e7 -G -1"
 #CPUEXTRA=-L
 
 hostid=$(hostname -s)
@@ -28,10 +28,10 @@ then
     if [ $# -eq 0 ] || [[ $1 =~ "cpu" ]]
     then    
 	echo testing benchmark B1 mmc mode with mmc
-	./run_test.sh -s b1_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b1_mmc.log
+	./run_test.sh -s b1_mmc $OPT -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b1_mmc.log
 
 	echo testing benchmark B1 dmmc mode with mmc
-	./run_tess.sh -s b1_dmmc $OPT -G -1 -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b1_dmmc.log
+	./run_tess.sh -s b1_dmmc $OPT -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b1_dmmc.log
     fi
 
     if [ $# -eq 0 ] || [[ $1 =~ "gpu" ]]
@@ -55,10 +55,10 @@ then
     if [ $# -eq 0 ] || [[ $1 =~ "cpu" ]]
     then   
 	echo testing benchmark B2 mmc mode with mmc
-	./run_mmc.sh -s b2_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b2_mmc.log
+	./run_mmc.sh -s b2_mmc $OPT -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b2_mmc.log
 
 	echo testing benchmark B2 dmmc mode with mmc
-	./run_dmmc.sh -s b2_dmmc $OPT -G -1 -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b2_dmmc.log
+	./run_dmmc.sh -s b2_dmmc $OPT -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b2_dmmc.log
     fi
 
     if [ $# -eq 0 ] || [[ $1 =~ "gpu" ]]
@@ -83,10 +83,10 @@ then
     if [ $# -eq 0 ] || [[ $1 =~ "cpu" ]]
     then   
 	echo testing benchmark B3 mmc mode with mmc
-	./run_test.sh -s b3_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b3_mmc.log
+	./run_test.sh -s b3_mmc $OPT -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b3_mmc.log
 
 	#echo testing benchmark B3 dmmc mode with mmc
-	#./run_test.sh -s b3_dmmc $OPT -G -1 -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b3_dmmc.log
+	#./run_test.sh -s b3_dmmc $OPT -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b3_dmmc.log
     fi
 
     if [ $# -eq 0 ] || [[ $1 =~ "gpu" ]]
@@ -110,10 +110,10 @@ then
     if [ $# -eq 0 ] || [[ $1 =~ "cpu" ]]
     then   
 	echo testing benchmark B4 mmc mode with mmc
-	./run_mmc.sh -s b4_mmc $OPT -G -1 -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b4_mmc.log
+	./run_mmc.sh -s b4_mmc $OPT -M S $EXTRA $CPUEXTRA > $OUTPUTDIR/b4_mmc.log
 
 	echo testing benchmark B4 dmmc mode with mmc
-	./run_dmmc.sh -s b4_dmmc $OPT -G -1 -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b4_dmmc.log
+	./run_dmmc.sh -s b4_dmmc $OPT -M G -F nii $EXTRA $CPUEXTRA > $OUTPUTDIR/b4_dmmc.log
     fi
 
     if [ $# -eq 0 ] || [[ $1 =~ "gpu" ]]
