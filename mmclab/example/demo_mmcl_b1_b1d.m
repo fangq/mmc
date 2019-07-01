5f58a5f51a064e827cef49d30f7184428b35dcd5
Qianqian Fang <fangqq@gmail.com>
2019-07-01 17:24:26 -0400

    update benchmark 4, correct alignment in benchmark 1

--- mmclab/example/demo_mmcl_b1_b1d.m
+++ mmclab/example/demo_mmcl_b1_b1d.m
@@ -54,10 +54,10 @@ c0=299792458000;
 twin=[cfg.tstart+cfg.tstep/2:cfg.tstep:cfg.tend];
 gates=length(twin);
 
-[xi,yi]=meshgrid(0:2:60,0:2:60);
+[xi,yi]=meshgrid(0.5:1:59.5,0.5:1:59.5);
 
-[cutpos1,cutvalue1,facedata1]=qmeshcut(cfg.elem(:,1:4),cfg.node,b1cpu,[0 30.2 0; 0 30.2 1; 1 30.2 0]);
-[cutpos2,cutvalue2,facedata2]=qmeshcut(cfg.elem(:,1:4),cfg.node,b1gpu,[0 30.2 0; 0 30.2 1; 1 30.2 0]);
+[cutpos1,cutvalue1,facedata1]=qmeshcut(cfg.elem(:,1:4),cfg.node,b1cpu,[0 30.5 0; 0 30.5 1; 1 30.5 0]);
+[cutpos2,cutvalue2,facedata2]=qmeshcut(cfg.elem(:,1:4),cfg.node,b1gpu,[0 30.5 0; 0 30.5 1; 1 30.5 0]);
 Phicpu=griddata(cutpos1(:,1),cutpos1(:,3),cutvalue1,xi,yi);
 Phigpu=griddata(cutpos2(:,1),cutpos2(:,3),cutvalue2,xi,yi);
 
@@ -83,22 +83,22 @@ flux=mmclab(cfg);
 Phidcpu=sum(flux.data,4);
 
 %%-----------------------------------------------------------------
-%% generate a contour plot along y=30.2
+%% generate a contour plot along y=30.5
 %%-----------------------------------------------------------------
 figure
 clines = 0:-0.5:-8;
-[xi,yi]=meshgrid(0:2:60,0:2:60);
+[xi,yi]=meshgrid(0.5:1:59.5,0.5:1:59.5);
 srcs=[30.1,30.2,0];
 dets=[xi(:) 30.2*ones(size(xi(:))) yi(:)];
 
 hold on
 [c h2]=contourf(xi,yi, log10(max(squeeze(Phicpu*cfg.tstep),1e-8)), clines, 'k-' );
 contour(xi,yi,log10(abs(Phigpu*cfg.tstep)),clines,'r:')
-contour(xi,yi,log10(abs(squeeze(Phidcpu(1:2:end,30,1:2:end))'*cfg.tstep)),clines,'b--')
-contour(xi,yi,log10(abs(squeeze(Phidgpu(1:2:end,30,1:2:end))'*cfg.tstep)),clines,'y--')
+contour(xi,yi,log10(abs(squeeze(Phidcpu(1:end-1,31,1:end-1))'*cfg.tstep)),clines,'b--')
+contour(xi,yi,log10(abs(squeeze(Phidgpu(1:end-1,31,1:end-1))'*cfg.tstep)),clines,'y--')
 
 axis equal  
-set(gca,'xlim',[1 60])
+set(gca,'xlim',[0.5 59.5])
 set(gca,'fontsize',20)
 xlabel('x (mm)')
 ylabel('z (mm)')
