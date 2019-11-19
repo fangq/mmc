a89457aac8949a359f4e3d9c87c15ad2e302033c
Qianqian Fang <fangqq@gmail.com>
2019-11-19 16:56:11 -0500

    fix time/weight scaling, see mcx bug #83

--- matlab/mmcdetweight.m
+++ matlab/mmcdetweight.m
@@ -1,6 +1,6 @@
-function detw=mmcdetweight(detp,prop)
+function detw=mmcdetweight(detp,prop,unitinmm)
 %
-% detw=mmcdetweight(detp,prop)
+% detw=mmcdetweight(detp,prop,unitinmm)
 %
 % Recalculate the detected photon weight using partial path data and 
 % optical properties (for perturbation Monte Carlo or detector readings)
@@ -10,19 +10,24 @@ function detw=mmcdetweight(detp,prop)
 % input:
 %     detp: the 2nd output from mmclab. detp must a struct 
 %     prop: optical property list, as defined in the cfg.prop field of mmclab's input
+%     unitinmm: voxel edge-length in mm, should use cfg.unitinmm used to generate detp; 
+%           if ignored, assume to be 1 (mm)
 %
 % output:
 %     detw: re-caculated detected photon weight based on the partial path data and optical property table
 %
 % this file is copied from Mesh-based Monte Carlo (MMC)
 %
-% License: GPLv3, see http://mcx.space/ for details
+% License: GPLv3, see http://mmc.space/ for details
 %
-
 
 medianum=size(prop,1);
 if(medianum<=1)
     error('empty property list');
+end
+
+if(nargin<=3)
+    unitinmm=1;
 end
 
 if(isstruct(detp))
@@ -32,7 +37,7 @@ if(isstruct(detp))
         detw=detp.w0;
     end
     for i=1:medianum-1
-        detw=detw.*exp(-prop(i+1,1)*detp.ppath(:,i));
+        detw=detw.*exp(-prop(i+1,1)*detp.ppath(:,i)*unitinmm);
     end
 else
     error('the first input must be a struct with a subfield named "ppath"');
