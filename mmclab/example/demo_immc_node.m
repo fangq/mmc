9bc35fcc6ba1cc2a75aa6fa7c094b1823c840a33
yaoshenyuan <yuan.yaos@husky.neu.edu>
2021-01-06 19:32:59 -0500

    Add n-immc demo and data

--- mmclab/example/demo_immc_node.m
+++ mmclab/example/demo_immc_node.m
@@ -5,15 +5,15 @@ load immc
 
 clear cfg
 
-cfg.implicit = 1;    % turn on edge-based immc
+cfg.implicit = 1;  % turn on node-based immc
 
 cfg.nphoton=1e6;
-cfg.node = node_eimmc;
-cfg.elem = elem_eimmc;
+cfg.node = node_nimmc;
+cfg.elem = elem_nimmc;
 cfg.elemprop=ones(size(cfg.elem,1),1);
 cfg.srcpos=[0.5 0.5 1];
 cfg.srcdir=[0 0 -1];
-cfg.prop=[0 0 1 1;0.0458 35.6541 0.9000 1.3700; 23.0543 9.3985 0.9000 1.3700];
+cfg.prop=[0 0 1 1;0.0458 35.6541 0.9000 1.3700;23.0543 9.3985 0.9000 1.3700];
 cfg.tstart=0;
 cfg.tend=5e-9;
 cfg.tstep=5e-9;
@@ -21,10 +21,10 @@ cfg.debuglevel='TP';
 cfg.method = 'grid';
 cfg.unitinmm = 0.01;
 cfg.steps = [0.01 0.01 0.01];
-cfg.isreflect = 1;
+cfg.isreflect=1;
 cfg.gpuid = -1;
 
-%% run edge-based iMMC
+%% run node-based iMMC
 flux=mmclab(cfg);
 
 %% plot the result
