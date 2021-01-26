47a9915d78fdd2a10b1c84e52f1d640b4ee178d5
yaoshenyuan <yuan.yaos@husky.neu.edu>
2021-01-26 14:13:56 -0500

    Modified the path to tetgen

--- mmclab/example/demo_immc_basic.m
+++ mmclab/example/demo_immc_basic.m
@@ -46,8 +46,7 @@
 
 %% add path
 % 1. you need to add the path to iso2mesh toolbox 
-% addpath('/path/to/iso2mesh/toolbox/');
-addpath(genpath('/space/neza/2/users/yaoshen/NEU/Research/iso2mesh/'))
+addpath('/path/to/iso2mesh/toolbox/');
 
 % 2. you need to add the path to MMC matlab folder
 addpath('../../matlab')
@@ -56,39 +55,41 @@ addpath('../../matlab')
 % load immc
 
 % (a) generate bounding box and insert edge
-[nbox,ebox] = meshgrid6(0:1,0:1,0:1);
-fbox = volface(ebox);
-EPS = 0.001;
-nbox = [nbox; [1-EPS 0.5 0.5]; [EPS 0.5 0.5]];  % insert new nodes (node 9 and 10)
-fbox = [fbox; [9 9 10]];  % insert new edge coneected by node 9 and 10
+[nbox,ebox]=meshgrid6(0:1,0:1,0:1);
+fbox=volface(ebox);
+EPS=0.001;
+nbox=[nbox; [1-EPS 0.5 0.5]; [EPS 0.5 0.5]];  % insert new nodes (node 9 and 10)
+fbox=[fbox; [9 9 10]];  % insert new edge coneected by node 9 and 10
 
 % (b) generate .poly file
 generatePoly('eimmc_mesh',nbox,fbox);
 
 % (c) generate mesh files using tetgen
 tic
-% !/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY vessel_mesh.poly
-!/space/neza/2/users/yaoshen/NEU/Research/iso2mesh/bin/tetgen1.5.mexa64 -YY eimmc_mesh.poly
+!/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY eimmc_mesh.poly
+if ~isfile('eimmc_mesh.1.node') || ~isfile('eimmc_mesh.1.face') || ~isfile('eimmc_mesh.1.ele')
+    error('Please identify your path to iso2mesh/bin/tetgen1.5.mexa64')
+end
 t=toc
 
 % (d) read and plot vessel mesh
-[node_eimmc, elem_eimmc, face_eimmc]=readMesh('eimmc_mesh');
+[node_eimmc,elem_eimmc,face_eimmc]=readMesh('eimmc_mesh');
 
 % (e) label the edge that has node 9 and 10 and add radii
-elem_eimmc = [elem_eimmc 6*ones(size(elem_eimmc))];
-elem_eimmc([10 11 13 16],5) = [5 4 4 3];  % local edge index
-elem_eimmc = [elem_eimmc zeros(size(elem_eimmc,1),4)];
-elem_eimmc([10 11 13 16],9) = [0.1 0.1 0.1 0.1];  % edge radius = 0.1
-node_eimmc = [node_eimmc zeros(size(node_eimmc,1),1)];
-node_eimmc(9,4) = 0.1;  % add radius for inserted node
+elem_eimmc=[elem_eimmc 6*ones(size(elem_eimmc))];
+elem_eimmc([10 11 13 16],5)=[5 4 4 3];  % local edge index
+elem_eimmc=[elem_eimmc zeros(size(elem_eimmc,1),4)];
+elem_eimmc([10 11 13 16],9)=[0.1 0.1 0.1 0.1];  % edge radius = 0.1
+node_eimmc=[node_eimmc zeros(size(node_eimmc,1),1)];
+node_eimmc(9,4)=0.1;  % add radius for inserted node
 
 clear cfg
 
-cfg.implicit = 1;    % turn on edge-based and node-based immc
+cfg.implicit=1;    % turn on edge-based and node-based immc
 
 cfg.nphoton=1e6;
-cfg.node = node_eimmc;
-cfg.elem = elem_eimmc;
+cfg.node=node_eimmc;
+cfg.elem=elem_eimmc;
 cfg.elemprop=ones(size(cfg.elem,1),1);
 cfg.srcpos=[0.5 0.5 1];
 cfg.srcdir=[0 0 -1];
@@ -97,10 +98,10 @@ cfg.tstart=0;
 cfg.tend=5e-9;
 cfg.tstep=5e-9;
 cfg.debuglevel='TP';
-cfg.method = 'grid';
-cfg.steps = [0.01 0.01 0.01];
-cfg.isreflect = 1;
-cfg.gpuid = -1;
+cfg.method='grid';
+cfg.steps=[0.01 0.01 0.01];
+cfg.isreflect=1;
+cfg.gpuid=-1;
 
 % run edge-based iMMC
 flux_eimmc=mmclab(cfg);
@@ -108,48 +109,50 @@ flux_eimmc=mmclab(cfg);
 %% node-based iMMC, benchmark B2
 
 % (a) generate bounding box and insert edge
-[nbox,ebox] = meshgrid6(0:1,0:1,0:1);
-fbox = volface(ebox);
-nbox = [nbox; [0.5 0.5 0.5]];  % insert new nodes (node 9)
+[nbox,ebox]=meshgrid6(0:1,0:1,0:1);
+fbox=volface(ebox);
+nbox=[nbox; [0.5 0.5 0.5]];  % insert new nodes (node 9)
 
 % (b) generate .poly file
 generatePoly('nimmc_mesh',nbox,fbox);
 
 % (c) generate mesh files using tetgen
 tic
-% !/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY vessel_mesh.poly
-!/space/neza/2/users/yaoshen/NEU/Research/iso2mesh/bin/tetgen1.5.mexa64 -YY nimmc_mesh.poly
+!/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY nimmc_mesh.poly
+if ~isfile(' nimmc_mesh.1.node') || ~isfile(' nimmc_mesh.1.face') || ~isfile(' nimmc_mesh.1.ele')
+    error('Please identify your path to iso2mesh/bin/tetgen1.5.mexa64')
+end
 t=toc
 
 % (d) read and plot vessel mesh
-[node_nimmc, elem_nimmc, face_nimmc]=readMesh('nimmc_mesh');
+[node_nimmc,elem_nimmc,face_nimmc]=readMesh('nimmc_mesh');
 
 % (e) label the edge that has node 9 and 10 and add radii
-elem_nimmc = [elem_nimmc 6*ones(size(elem_nimmc)) zeros(size(elem_nimmc))];
-node_nimmc = [node_nimmc zeros(size(node_nimmc,1),1)];
-node_nimmc(9,4) = 0.1;  % add radius for inserted node
+elem_nimmc=[elem_nimmc 6*ones(size(elem_nimmc)) zeros(size(elem_nimmc))];
+node_nimmc=[node_nimmc zeros(size(node_nimmc,1),1)];
+node_nimmc(9,4)=0.1;  % add radius for inserted node
 
 % run node-based iMMC
-cfg.elem = elem_nimmc;
-cfg.node = node_nimmc;
+cfg.elem=elem_nimmc;
+cfg.node=node_nimmc;
 flux_nimmc=mmclab(cfg);
 
 
 %% face-based iMMC, benchmark B3
 
-cfg.implicit = 2;  % set cfg.implicit to 2 to turn on f-iMMC
+cfg.implicit=2;  % set cfg.implicit to 2 to turn on f-iMMC
 
 % (a) generate bounding box and insert edge
-[nbox,ebox] = meshgrid6(0:1,0:1,0:1);
-fbox = volface(ebox);
+[nbox,ebox]=meshgrid6(0:1,0:1,0:1);
+fbox=volface(ebox);
 
-elem_fimmc = [ebox 6*ones(size(ebox)) zeros(size(ebox))];
-elem_fimmc([3 4 5 6],5) = [-4 1 -6 1];
-elem_fimmc([3 4 5 6],9) = [0.1 0.1 0.1 0.1];
-node_fimmc = [nbox zeros(size(nbox,1),1)];
+elem_fimmc=[ebox 6*ones(size(ebox)) zeros(size(ebox))];
+elem_fimmc([3 4 5 6],5)=[-4 1 -6 1];
+elem_fimmc([3 4 5 6],9)=[0.1 0.1 0.1 0.1];
+node_fimmc=[nbox zeros(size(nbox,1),1)];
 
-cfg.node = node_fimmc;
-cfg.elem = elem_fimmc;
+cfg.node=node_fimmc;
+cfg.elem=elem_fimmc;
 
 % run node-based iMMC
 flux_fimmc=mmclab(cfg);
@@ -165,14 +168,14 @@ function generatePoly(sessionid,node,face)
 % generate the .poly file in current folder
 fid=fopen([sessionid,'.poly'],'wt');
 % === node ===
-[nn,nd] = size(node);
+[nn,nd]=size(node);
 fprintf(fid,'#node list\n');
 fprintf(fid,'%d %d %d %d\n',nn,nd,0,0);
-fprintf(fid,'%d %6.16f %6.16f %6.16f\n',[0:nn-1; node(:,1)'; node(:,2)'; node(:,3)']);
+fprintf(fid,'%d %6.16f %6.16f %6.16f\n',[0:nn-1;node(:,1)';node(:,2)';node(:,3)']);
 
 % === face ===
-[ne,~] = size(face);
-face = face-1;
+[ne,~]=size(face);
+face=face-1;
 fprintf(fid,'#face list\n');
 fprintf(fid,'%d %d\n',ne,1);
 for i=1:ne
@@ -191,27 +194,27 @@ end
 function [node_tet,elem_tet,face_tet]=readMesh(sessionid)
 % read the .elem, .face and .node files generated from tetgen
 % and write into elem_tet, face_tet and node_tet in MATLAB
-fileID = fopen([sessionid '.1.ele'],'r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-A = [A(1:3); 0; 0; A(4:end)];
-elem_tet = reshape(A,[5 A(1)+1])';
+fileID=fopen([sessionid '.1.ele'],'r');
+formatSpec='%f';
+A=fscanf(fileID,formatSpec);
+A=[A(1:3); 0; 0; A(4:end)];
+elem_tet=reshape(A,[5 A(1)+1])';
 
-fileID = fopen([sessionid '.1.face'],'r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-A = [A(1:2); 0; 0; 0; A(3:end)];
-face_tet = reshape(A,[5 A(1)+1])';
+fileID=fopen([sessionid '.1.face'],'r');
+formatSpec='%f';
+A=fscanf(fileID,formatSpec);
+A=[A(1:2); 0; 0; 0; A(3:end)];
+face_tet=reshape(A,[5 A(1)+1])';
 
-fileID = fopen([sessionid '.1.node'],'r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-node_tet = reshape(A,[4 A(1)+1])';
+fileID=fopen([sessionid '.1.node'],'r');
+formatSpec='%f';
+A=fscanf(fileID,formatSpec);
+node_tet=reshape(A,[4 A(1)+1])';
 
-elem_tet = elem_tet(2:end,2:end);
-elem_tet = elem_tet+1;
-face_tet = face_tet(2:end,2:end);
-face_tet = face_tet+1;
-node_tet = node_tet(2:end,2:end);
+elem_tet=elem_tet(2:end,2:end);
+elem_tet=elem_tet+1;
+face_tet=face_tet(2:end,2:end);
+face_tet=face_tet+1;
+node_tet=node_tet(2:end,2:end);
 
 end
--- mmclab/example/demo_immc_vessel.m
+++ mmclab/example/demo_immc_vessel.m
@@ -7,8 +7,7 @@
 % nbox:   nodes of bounding box
 % fbox:   faces of bounding box
 
-% addpath('/path/to/iso2mesh/toolbox/');
-addpath(genpath('/space/neza/2/users/yaoshen/NEU/Research/iso2mesh/'))
+addpath('/path/to/iso2mesh/toolbox/');
 addpath('../../matlab')
 
 load vessel
@@ -16,20 +15,22 @@ load vessel
 %% 1. Mesh generation
 
 % (a) add the bounding box (nbox and fbox) into node and face
-offset = size(nbox,1);
-edge = edge+offset;  % change the node index after inserting bounding box
-noder = [zeros(offset,1); noder];  % add offset into noder
-node = [nbox; node];  % add nodes of bounding box into node
-fedge = [edge(:,1) edge];  %  convert edge to face for mesh generation
-face = [fbox; fedge];  %  add faces of bounding box into face
+offset=size(nbox,1);
+edge=edge+offset;  % change the node index after inserting bounding box
+noder=[zeros(offset,1); noder];  % add offset into noder
+node=[nbox; node];  % add nodes of bounding box into node
+fedge=[edge(:,1) edge];  %  convert edge to face for mesh generation
+face=[fbox; fedge];  %  add faces of bounding box into face
 
 % (b) generate .poly file
 generatePoly('vessel_mesh',node,face);
 
 % (c) generate mesh files using tetgen
 tic
-% !/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY vessel_mesh.poly
-!/space/neza/2/users/yaoshen/NEU/Research/iso2mesh/bin/tetgen1.5.mexa64 -YY vessel_mesh.poly
+!/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY vessel_mesh.poly
+if ~isfile('vessel_mesh.1.node') || ~isfile('vessel_mesh.1.face') || ~isfile('vessel_mesh.1.ele')
+    error('Please identify your path to iso2mesh/bin/tetgen1.5.mexa64')
+end
 t=toc
 
 % (d) read and plot vessel mesh
@@ -38,29 +39,29 @@ t=toc
 %% 2. Label local edges
 
 % 1. label the local edges which are vessels for all elements
-[localEdgeIndex, edgeSet] = labelElements(edge,elemVessel);
+[localEdgeIndex, edgeSet]=labelElements(edge,elemVessel);
 
 % 2. split the elements wich over 2 edges labeled as vessel
-indexForSplit = find(sum(logical(localEdgeIndex),2)>2);
+indexForSplit=find(sum(logical(localEdgeIndex),2)>2);
 if ~isempty(indexForSplit)
-    [nodeVessel,elemVessel,localEdgeIndex] = relabelElement(elemVessel,localEdgeIndex,indexForSplit,nodeVessel,edgeSet);
-    noder = [noder; zeros(size(nodeVessel,1)-size(noder,1),1)];
+    [nodeVessel,elemVessel,localEdgeIndex]=relabelElement(elemVessel,localEdgeIndex,indexForSplit,nodeVessel,edgeSet);
+    noder=[noder; zeros(size(nodeVessel,1)-size(noder,1),1)];
 end
 
 % 3. get radius for all edges (comment this line if you have your own vessel radii)
-radii = getVesselRadii(elemVessel,localEdgeIndex,noder);
+radii=getVesselRadii(elemVessel,localEdgeIndex,noder);
 
 %% 3. Run mmc
 
 clear cfg
 
-cfg = inputMesh2Cfg(elemVessel,localEdgeIndex,radii,nodeVessel,noder);
+cfg=inputMesh2Cfg(elemVessel,localEdgeIndex,radii,nodeVessel,noder);
 
-res = 0.001;  % resolution: 0.001mm
-cfg.steps = [0.001 0.001 0.001];
-cfg.elem(:,9:12) = cfg.elem(:,9:12)*res;
-cfg.node = cfg.node*res;
-cfg.srctype = 'pencil';
+res=0.001;  % resolution: 0.001mm
+cfg.steps=[0.001 0.001 0.001];
+cfg.elem(:,9:12)=cfg.elem(:,9:12)*res;
+cfg.node=cfg.node*res;
+cfg.srctype='pencil';
 cfg.nphoton=1e6;
 cfg.elemprop=ones(size(cfg.elem,1),1);
 cfg.srcpos=[150.5 150.5 605]*res;
@@ -72,25 +73,27 @@ cfg.tstart=0;
 cfg.tend=5e-9;
 cfg.tstep=5e-9;
 cfg.debuglevel='TP';
-cfg.method = 'grid';
-cfg.gpuid = -1;
-cfg.isreflect = 1;
-cfg.outputtype = 'energy';
+cfg.method='grid';
+cfg.gpuid=-1;
+cfg.isreflect=1;
+cfg.outputtype='energy';
 
 tic
 flux=mmclab(cfg);
-runtime = toc
+runtime=toc
 
 figure,imagesc(log10(rot90(squeeze(flux.data(:,150,:)))))
+
+
 
 %% %%%%%%%%%%% functions %%%%%%%%%%%
 
 %% function: plot edges
 function plotEdges(edge,node)
 for i=1:size(edge,1)
-    plot3([node(edge(i,1),1), node(edge(i,2),1)],...
-        [node(edge(i,1),2), node(edge(i,2),2)],...
-        [node(edge(i,1),3), node(edge(i,2),3)],'r');
+    plot3([node(edge(i,1),1),node(edge(i,2),1)],...
+        [node(edge(i,1),2),node(edge(i,2),2)],...
+        [node(edge(i,1),3),node(edge(i,2),3)],'r');
     hold on
 end
 end
@@ -98,14 +101,14 @@ end
 %% function: compute element centroid
 function centroid = elemcentroid(node,elem)
 % compute the centroid of all elements
-centroid = zeros(size(elem,1),3);
+centroid=zeros(size(elem,1),3);
 for i=1:size(elem,1)
-    n1 = node(elem(i,1),:);
-    n2 = node(elem(i,2),:);
-    n3 = node(elem(i,3),:);
-    n4 = node(elem(i,4),:);
-    n = [n1; n2; n3; n4];
-    centroid(i,:) = mean(n);
+    n1=node(elem(i,1),:);
+    n2=node(elem(i,2),:);
+    n3=node(elem(i,3),:);
+    n4=node(elem(i,4),:);
+    n=[n1; n2; n3; n4];
+    centroid(i,:)=mean(n);
 end
 
 end
@@ -115,14 +118,14 @@ function generatePoly(sessionid,node,face)
 % generate the .poly file in current folder
 fid=fopen([sessionid,'.poly'],'wt');
 % === node ===
-[nn,nd] = size(node);
+[nn,nd]=size(node);
 fprintf(fid,'#node list\n');
 fprintf(fid,'%d %d %d %d\n',nn,nd,0,0);
-fprintf(fid,'%d %6.16f %6.16f %6.16f\n',[0:nn-1; node(:,1)'; node(:,2)'; node(:,3)']);
+fprintf(fid,'%d %6.16f %6.16f %6.16f\n',[0:nn-1;node(:,1)';node(:,2)';node(:,3)']);
 
 % === face ===
-[ne,~] = size(face);
-face = face-1;
+[ne,~]=size(face);
+face=face-1;
 fprintf(fid,'#face list\n');
 fprintf(fid,'%d %d\n',ne,1);
 for i=1:ne
@@ -141,42 +144,42 @@ end
 function [node_tet,elem_tet,face_tet]=readMesh(sessionid)
 % read the .elem, .face and .node files generated from tetgen
 % and write into elem_tet, face_tet and node_tet in MATLAB
-fileID = fopen([sessionid '.1.ele'],'r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-A = [A(1:3); 0; 0; A(4:end)];
-elem_tet = reshape(A,[5 A(1)+1])';
+fileID=fopen([sessionid '.1.ele'],'r');
+formatSpec='%f';
+A=fscanf(fileID,formatSpec);
+A=[A(1:3); 0; 0; A(4:end)];
+elem_tet=reshape(A,[5 A(1)+1])';
 
-fileID = fopen([sessionid '.1.face'],'r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-A = [A(1:2); 0; 0; 0; A(3:end)];
-face_tet = reshape(A,[5 A(1)+1])';
+fileID=fopen([sessionid '.1.face'],'r');
+formatSpec='%f';
+A=fscanf(fileID,formatSpec);
+A=[A(1:2); 0; 0; 0; A(3:end)];
+face_tet=reshape(A,[5 A(1)+1])';
 
-fileID = fopen([sessionid '.1.node'],'r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-node_tet = reshape(A,[4 A(1)+1])';
+fileID=fopen([sessionid '.1.node'],'r');
+formatSpec='%f';
+A=fscanf(fileID,formatSpec);
+node_tet=reshape(A,[4 A(1)+1])';
 
-elem_tet = elem_tet(2:end,2:end);
-elem_tet = elem_tet+1;
-face_tet = face_tet(2:end,2:end);
-face_tet = face_tet+1;
-node_tet = node_tet(2:end,2:end);
+elem_tet=elem_tet(2:end,2:end);
+elem_tet=elem_tet+1;
+face_tet=face_tet(2:end,2:end);
+face_tet=face_tet+1;
+node_tet=node_tet(2:end,2:end);
 
 end
 
 %% function: label local edges for all elements
-function [localEdgeIndex,edgeSet] = labelElements(edge,elem)
+function [localEdgeIndex,edgeSet]=labelElements(edge,elem)
 % label the local edges for all input element
-n2e = [1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
-edge = sort(edge,2);          % sort to unify the edge representation
-edgeSet = java.util.HashSet;  % set storing all vessel edges
+n2e=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
+edge=sort(edge,2);          % sort to unify the edge representation
+edgeSet=java.util.HashSet;  % set storing all vessel edges
 for i=1:size(edge,1)
     edgeSet.add(num2str(edge(i,:)));
 end
 
-localEdgeIndex = zeros(size(elem,1),size(n2e,1));
+localEdgeIndex=zeros(size(elem,1),size(n2e,1));
 fprintf('Getting local edge indices for each element...\n')
 for i=1:size(elem,1)
    c = 1;
@@ -194,124 +197,124 @@ fprintf('Finished index extraction.\n')
 end
 
 %% function: relabel the elements
-function [node,elemInserted,edgeIndexInserted] = relabelElement(elem,edgeIndex,indexForSplit,node,edgeSet)
+function [node,elemInserted,edgeIndexInserted]=relabelElement(elem,edgeIndex,indexForSplit,node,edgeSet)
 
 fprintf('Relabeling elements with labeled edges larger than 2...\n')
-faceorder = [1 2 3; 1 2 4; 1 3 4; 2 3 4];
+faceorder=[1 2 3; 1 2 4; 1 3 4; 2 3 4];
 
 % get elements that need to be split
-elemSplit = elem(indexForSplit,:);
+elemSplit=elem(indexForSplit,:);
 
 % get centroids and add them into nodes
-centroid = elemcentroid(node,elemSplit);
-offset = size(node,1);
-node = [node; centroid];
+centroid=elemcentroid(node,elemSplit);
+offset=size(node,1);
+node=[node; centroid];
 
 %% get new elements and new labelings
 
 % 4 new elements will be generated for each element to be split
-splitElem = cell(length(indexForSplit),1);  % store new elements after spliting
-splitLocalEdge = cell(length(indexForSplit),1);  % store local edge labeling for the new elements
+splitElem=cell(length(indexForSplit),1);  % store new elements after spliting
+splitLocalEdge=cell(length(indexForSplit),1);  % store local edge labeling for the new elements
 for i=1:size(elemSplit,1)
     % one element can be split into 4 new elements
-    newElem = zeros(size(faceorder,1),4);  % 4 nodes in an element
-    newLocalEdge = zeros(size(faceorder,1),6);  % 6 edges in an element
+    newElem=zeros(size(faceorder,1),4);  % 4 nodes in an element
+    newLocalEdge=zeros(size(faceorder,1),6);  % 6 edges in an element
     for j=1:size(faceorder,1)
         % offset+i is the node index of the centroid
-        newElem(j,:) = [offset+i,...
+        newElem(j,:)=[offset+i,...
                         elemSplit(i,faceorder(j,1)),...
                         elemSplit(i,faceorder(j,2)),...
                         elemSplit(i,faceorder(j,3))];
-        newLocalEdge(j,:) = labelSingleElem(newElem(j,:),edgeSet);
+        newLocalEdge(j,:)=labelSingleElem(newElem(j,:),edgeSet);
         if sum(logical(newLocalEdge(j,:)))>2
            warning('Split failed')
         end
     end
-    splitElem{i} = newElem;
-    splitLocalEdge{i} = newLocalEdge;
+    splitElem{i}=newElem;
+    splitLocalEdge{i}=newLocalEdge;
 end
 
 %% Insert new elements to original elements
 
 if indexForSplit(1)==1
-    elemInserted = [];
-    edgeIndexInserted = [];
+    elemInserted=[];
+    edgeIndexInserted=[];
 else
-    elemInserted = elem(1:indexForSplit(1)-1,:);
-    edgeIndexInserted = edgeIndex(1:indexForSplit(1)-1,:);
+    elemInserted=elem(1:indexForSplit(1)-1,:);
+    edgeIndexInserted=edgeIndex(1:indexForSplit(1)-1,:);
 end
-elemInserted = [elemInserted; splitElem{1}];
-edgeIndexInserted = [edgeIndexInserted; splitLocalEdge{1}];
+elemInserted=[elemInserted; splitElem{1}];
+edgeIndexInserted=[edgeIndexInserted; splitLocalEdge{1}];
 
 for i=2:length(indexForSplit)
-    elemInserted = [elemInserted; elem(indexForSplit(i-1)+1:indexForSplit(i)-1,:)];
-    elemInserted = [elemInserted; splitElem{i}];
+    elemInserted=[elemInserted; elem(indexForSplit(i-1)+1:indexForSplit(i)-1,:)];
+    elemInserted=[elemInserted; splitElem{i}];
     
-    edgeIndexInserted = [edgeIndexInserted; edgeIndex(indexForSplit(i-1)+1:indexForSplit(i)-1,:)];
-    edgeIndexInserted = [edgeIndexInserted; splitLocalEdge{i}];
+    edgeIndexInserted=[edgeIndexInserted; edgeIndex(indexForSplit(i-1)+1:indexForSplit(i)-1,:)];
+    edgeIndexInserted=[edgeIndexInserted; splitLocalEdge{i}];
 end
 
 if indexForSplit(end)<size(elem,1)
-    elemInserted = [elemInserted; elem(indexForSplit(end)+1:end,:)];
-    edgeIndexInserted = [edgeIndexInserted; edgeIndex(indexForSplit(end)+1:end,:)];
+    elemInserted=[elemInserted; elem(indexForSplit(end)+1:end,:)];
+    edgeIndexInserted=[edgeIndexInserted; edgeIndex(indexForSplit(end)+1:end,:)];
 end
 fprintf('Finished relabeling.\n')
 
 end
 
 %% function: label local edges for single element
-function localEdge = labelSingleElem(elem,edgeSet)
+function localEdge=labelSingleElem(elem,edgeSet)
 % label a single element
-n2e = [1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
-localEdge = zeros(1,size(n2e,1));
-c = 1;
+n2e=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
+localEdge=zeros(1,size(n2e,1));
+c=1;
 for i=1:size(n2e,1)
     % check if the edge is in the set
-    curEdge = sort([elem(n2e(i,1)) elem(n2e(i,2))],2);
+    curEdge=sort([elem(n2e(i,1)) elem(n2e(i,2))],2);
     if edgeSet.contains(num2str(curEdge))
-        localEdge(c) = i;
-        c = c+1;
+        localEdge(c)=i;
+        c=c+1;
     end
 end
 
 end
 
 %% function: obtain the radii for the labeled edges
-function radii = getVesselRadii(elem,index,noder)
-n2e = [1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
-radii = zeros(size(index,1),2);
+function radii=getVesselRadii(elem,index,noder)
+n2e=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
+radii=zeros(size(index,1),2);
 for i=1:size(index,1)
     % edge 1
     if index(i,1)~=0
-        ei1 = n2e(index(i,1),:);
-        radii(i,1) = max([noder(elem(i,ei1(1))),noder(elem(i,ei1(2)))]);
+        ei1=n2e(index(i,1),:);
+        radii(i,1)=max([noder(elem(i,ei1(1))),noder(elem(i,ei1(2)))]);
     end
     
     % edge 2
     if index(i,2)~=0
-        ei2 = n2e(index(i,2),:);
-        radii(i,2) = max([noder(elem(i,ei2(1))),noder(elem(i,ei2(2)))]);
+        ei2=n2e(index(i,2),:);
+        radii(i,2)=max([noder(elem(i,ei2(1))),noder(elem(i,ei2(2)))]);
     end
 end
 
 end
 
 %% function: build cfg for immc
-function cfg = inputMesh2Cfg(elem,index,radii,node,noder)
+function cfg=inputMesh2Cfg(elem,index,radii,node,noder)
 
 % turn on iMMC
-cfg.implicit = 1;
+cfg.implicit=1;
 
 % post-processing to fit to iMMC input format
-index = index(:,1:4);
-index(index==0) = 7;
+index=index(:,1:4);
+index(index==0)=7;
 % 0 to 5 represet local edge labeled as vessel, 6 represent not labeled as
 % vessel
-index = index-1;
+index=index-1;
 
-node = [node noder];
-elem = [elem index radii zeros(size(radii,1),2)];
+node=[node noder];
+elem=[elem index radii zeros(size(radii,1),2)];
 
-cfg.node = node;
-cfg.elem = elem;
+cfg.node=node;
+cfg.elem=elem;
 end
