46183d890437ff43fe3cdf6e5086101f3ece5b38
Qianqian Fang <fangqq@gmail.com>
2021-03-02 17:58:33 -0500

    remove unnecessarily duplicated functions, use iso2mesh functions

--- mmclab/example/demo_immc_basic.m
+++ mmclab/example/demo_immc_basic.m
@@ -46,7 +46,7 @@
 
 %% add path
 % 1. you need to add the path to iso2mesh toolbox 
-addpath('/path/to/iso2mesh/toolbox/');
+%addpath('/path/to/iso2mesh/toolbox/');
 
 % 2. you need to add the path to MMC matlab folder
 addpath('../../matlab')
@@ -62,18 +62,18 @@ nbox=[nbox; [1-EPS 0.5 0.5]; [EPS 0.5 0.5]];  % insert new nodes (node 9 and 10)
 fbox=[fbox; [9 9 10]];  % insert new edge coneected by node 9 and 10
 
 % (b) generate .poly file
-generatePoly('eimmc_mesh',nbox,fbox);
+savesurfpoly(nbox,num2cell(fbox,2),[],[],[],[],mwpath('eimmc_mesh.poly'));
 
 % (c) generate mesh files using tetgen
 tic
-!/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY eimmc_mesh.poly
-if ~isfile('eimmc_mesh.1.node') || ~isfile('eimmc_mesh.1.face') || ~isfile('eimmc_mesh.1.ele')
-    error('Please identify your path to iso2mesh/bin/tetgen1.5.mexa64')
+[status, cmdout]=system([' "' mcpath('tetgen1.5',getexeext) '" -YY "' mwpath('eimmc_mesh.poly') '"']);
+if(status)
+    error('tetgen error: %s\n',cmdout);
 end
-t=toc
+toc
 
 % (d) read and plot vessel mesh
-[node_eimmc,elem_eimmc,face_eimmc]=readMesh('eimmc_mesh');
+[node_eimmc,elem_eimmc,face_eimmc]=readtetgen(mwpath('eimmc_mesh.1'));
 
 % (e) label the edge that has node 9 and 10 and add radii
 elem_eimmc=[elem_eimmc 6*ones(size(elem_eimmc))];
@@ -114,18 +114,18 @@ fbox=volface(ebox);
 nbox=[nbox; [0.5 0.5 0.5]];  % insert new nodes (node 9)
 
 % (b) generate .poly file
-generatePoly('nimmc_mesh',nbox,fbox);
+savesurfpoly(nbox,num2cell(fbox,2),[],[],[],[],mwpath('nimmc_mesh.poly'));
 
 % (c) generate mesh files using tetgen
 tic
-!/path/to/your/iso2mesh/bin/tetgen1.5.mexa64 -YY nimmc_mesh.poly
-if ~isfile(' nimmc_mesh.1.node') || ~isfile(' nimmc_mesh.1.face') || ~isfile(' nimmc_mesh.1.ele')
-    error('Please identify your path to iso2mesh/bin/tetgen1.5.mexa64')
+[status, cmdout]=system([' "' mcpath('tetgen1.5',getexeext) '" -YY "' mwpath('nimmc_mesh.poly') '"']);
+if(status)
+    error('tetgen error: %s\n',cmdout);
 end
-t=toc
+toc
 
 % (d) read and plot vessel mesh
-[node_nimmc,elem_nimmc,face_nimmc]=readMesh('nimmc_mesh');
+[node_nimmc,elem_nimmc,face_nimmc]=readtetgen(mwpath('nimmc_mesh.1'));
 
 % (e) label the edge that has node 9 and 10 and add radii
 elem_nimmc=[elem_nimmc 6*ones(size(elem_nimmc)) zeros(size(elem_nimmc))];
@@ -162,59 +162,3 @@ figure,
 subplot(131),imagesc(log10(rot90(squeeze(flux_eimmc.data(50,1:100,1:100)))))
 subplot(132),imagesc(log10(rot90(squeeze(flux_nimmc.data(50,1:100,1:100)))))
 subplot(133),imagesc(log10(rot90(squeeze(flux_fimmc.data(50,1:100,1:100)))))
-
-%% function: generate .poly
-function generatePoly(sessionid,node,face)
-% generate the .poly file in current folder
-fid=fopen([sessionid,'.poly'],'wt');
-% === node ===
-[nn,nd]=size(node);
-fprintf(fid,'#node list\n');
-fprintf(fid,'%d %d %d %d\n',nn,nd,0,0);
-fprintf(fid,'%d %6.16f %6.16f %6.16f\n',[0:nn-1;node(:,1)';node(:,2)';node(:,3)']);
-
-% === face ===
-[ne,~]=size(face);
-face=face-1;
-fprintf(fid,'#face list\n');
-fprintf(fid,'%d %d\n',ne,1);
-for i=1:ne
-    fprintf(fid,'%d %d\n',1,0);
-    fprintf(fid,'%d %d %d %d\n',3,face(i,1),face(i,2),face(i,3));
-end
-
-% === hole ===
-fprintf(fid,'#hole list\n');
-fprintf(fid,'%d\n',0);
-
-fclose(fid);
-end
-
-%% function: read generated .elem, .face and .node files
-function [node_tet,elem_tet,face_tet]=readMesh(sessionid)
-% read the .elem, .face and .node files generated from tetgen
-% and write into elem_tet, face_tet and node_tet in MATLAB
-fileID=fopen([sessionid '.1.ele'],'r');
-formatSpec='%f';
-A=fscanf(fileID,formatSpec);
-A=[A(1:3); 0; 0; A(4:end)];
-elem_tet=reshape(A,[5 A(1)+1])';
-
-fileID=fopen([sessionid '.1.face'],'r');
-formatSpec='%f';
-A=fscanf(fileID,formatSpec);
-A=[A(1:2); 0; 0; 0; A(3:end)];
-face_tet=reshape(A,[5 A(1)+1])';
-
-fileID=fopen([sessionid '.1.node'],'r');
-formatSpec='%f';
-A=fscanf(fileID,formatSpec);
-node_tet=reshape(A,[4 A(1)+1])';
-
-elem_tet=elem_tet(2:end,2:end);
-elem_tet=elem_tet+1;
-face_tet=face_tet(2:end,2:end);
-face_tet=face_tet+1;
-node_tet=node_tet(2:end,2:end);
-
-end
