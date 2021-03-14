29b9fda809b333d338e8798f1767cbe3e42f6d5d
yaoshenyuan <yuan.yaos@husky.neu.edu>
2021-03-14 18:29:25 -0400

    Modified vessel demo

--- mmclab/example/demo_immc_vessel.m
+++ mmclab/example/demo_immc_vessel.m
@@ -33,36 +33,23 @@ if(nargin==0)
     face=[fbox; fedge];  %  add faces of bounding box into face
 
     % (b) generate .poly file
-    [nodeVessel, elemVessel]=s2m(node,num2cell(face,2),1,100,'tetgen1.5',[],[],'-YY');
+    % vnode, velem: nodes and elements for generated mesh
+    [vnode, velem]=s2m(node,num2cell(face,2),1,100,'tetgen1.5',[],[],'-YY');
 
-    %% 2. Label local edges
-
-    % 1. label the local edges which are vessels for all elements
-    [localEdgeIndex, edgeSet]=labelElements(edge,elemVessel);
-
-    % 2. split the elements wich over 2 edges labeled as vessel
-    indexForSplit=find(sum(logical(localEdgeIndex),2)>2);
-    if ~isempty(indexForSplit)
-        [nodeVessel,elemVessel,localEdgeIndex]=relabelElement(elemVessel,localEdgeIndex,indexForSplit,nodeVessel,edgeSet);
-        noder=[noder; zeros(size(nodeVessel,1)-size(noder,1),1)];
-    end
-
-    % 3. get radius for all edges (comment this line if you have your own vessel radii)
-    radii=getVesselRadii(elemVessel,localEdgeIndex,noder);
+    %% 2. Create edgeroi
+    % (a) get mapping from edge to radius (you can build your own e2r)
+    e2r=edge2radius(edge,noder);
+    % (b) get edgeroi
+    [vnode,velem,eroi]=edgeroi(vnode,velem,e2r);
 
     %% 3. Run mmc
 
-    cfg=inputMesh2Cfg(elemVessel,localEdgeIndex,radii,nodeVessel,noder);
-
     res=0.001;  % resolution: 0.001mm
-    cfg.steps=[0.001 0.001 0.001];
-    cfg.elem(:,9:12)=cfg.elem(:,9:12)*res;
-    cfg.node=cfg.node*res;
-    cfg.srctype='pencil';
     cfg.nphoton=1e6;
-    cfg.elemprop=ones(size(cfg.elem,1),1);
+    cfg.srctype='pencil';
     cfg.srcpos=[150.5 150.5 605]*res;
     cfg.srcdir=[0 0 -1];
+    cfg.elemprop=ones(size(velem,1),1);
     cfg.prop=[0 0 1 1;
         0.0458   35.6541    0.9000    1.3700;   % dermis
         23.0543    9.3985    0.9000    1.3700]; % blood
@@ -71,10 +58,16 @@ if(nargin==0)
     cfg.tstep=5e-9;
     cfg.debuglevel='TP';
     cfg.method='grid';
+    cfg.steps=[0.001 0.001 0.001];
     cfg.gpuid=-1;
     cfg.isreflect=1;
     cfg.outputtype='energy';
-
+    
+    cfg.elem=velem;
+    cfg.node=vnode*res;
+    cfg.edgeroi=eroi*res;
+    cfg.noderoi=zeros(size(vnode,1),1);
+%     cfg.noderoi(1:size(noder,1))=noder;
 end
 
 tic
@@ -87,148 +80,112 @@ axis equal;
 title('energy deposition map');
 
 
-%% %%%%%%%%%%% functions %%%%%%%%%%%
+%% function: get vessel radii
+function e2r = edge2radius(edge,noder)
 
-%% function: label local edges for all elements
-function [localEdgeIndex,edgeSet]=labelElements(edge,elem)
-% label the local edges for all input element
-n2e=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
-edge=sort(edge,2);          % sort to unify the edge representation
-edgeSet=java.util.HashSet;  % set storing all vessel edges
+% vr = vesselradii(edge,noder)
+% 
+% get the radii for the input edges (vessels)
+% 
+% input:
+%     edge: ne x 2 array. ne is the total number of edges. 2 columns represent
+%     the indices of nodes connecting the edges
+%     noder: nr x 1 array storing the radius of each node. nr is the total 
+%     number of nodes
+% output:
+%     vr: ne x 1 array storing the radius of each edge
+
+e2r=java.util.HashMap;  % set storing all vessel edges
+edge=sort(edge,2);
 for i=1:size(edge,1)
-    edgeSet.add(num2str(edge(i,:)));
+    vr=max([noder(edge(i,1)),noder(edge(i,2))]);
+    e2r.put(num2str(edge(i,:)),vr);
 end
 
-localEdgeIndex=zeros(size(elem,1),size(n2e,1));
-fprintf('Getting local edge indices for each element...\n')
-for i=1:size(elem,1)
-   c = 1;
-   for j=1:size(n2e,1)
-       % check is local edge is in the edgeSet
-       curLocalEdge = sort([elem(i,n2e(j,1)),elem(i,n2e(j,2))],2);
-       if edgeSet.contains(num2str(curLocalEdge))
-           localEdgeIndex(i,c) = j;
-           c = c+1;
-       end
-   end
-end
-fprintf('Finished index extraction.\n')
+%% function: get edgeroi
+function [node,elem,eroi]=edgeroi(node,elem,e2r)
+% 
+% build the edgeroi for iMMC
+% 
+% input:
+%     node: node coordinates for elem
+%     elem: input mesh containing the edges
+%     e2r: java.util.HashMap mapping edge to its radius
+%          <key, value> -> <edge, radius>
+%          Example:
+%          edge=[1 2], radius=0.1
+%          To build the mapping, you should do
+%          e2r.put(num2str(edge),radius)
+% output:
+%     eroi: ne x 6 array, storing the cylindrial edge-roi radii in the 
+%           nchoosek order [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]. 
+%           A 0 indicates no ROI. Up to 2 edges are supported
 
+eo=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4]; % edge order in an element
+eroi = zeros(size(elem,1),6);
 
-%% function: relabel the elements
-function [node,elemInserted,edgeIndexInserted]=relabelElement(elem,edgeIndex,indexForSplit,node,edgeSet)
-
-fprintf('Relabeling elements with labeled edges larger than 2...\n')
-faceorder=[1 2 3; 1 2 4; 1 3 4; 2 3 4];
-
-% get elements that need to be split
-elemSplit=elem(indexForSplit,:);
-
-% get centroids and add them into nodes
-centroid=meshcentroid(node,elemSplit);
-offset=size(node,1);
-node=[node; centroid];
-
-%% get new elements and new labelings
-
-% 4 new elements will be generated for each element to be split
-splitElem=cell(length(indexForSplit),1);  % store new elements after spliting
-splitLocalEdge=cell(length(indexForSplit),1);  % store local edge labeling for the new elements
-for i=1:size(elemSplit,1)
-    % one element can be split into 4 new elements
-    newElem=zeros(size(faceorder,1),4);  % 4 nodes in an element
-    newLocalEdge=zeros(size(faceorder,1),6);  % 6 edges in an element
-    for j=1:size(faceorder,1)
-        % offset+i is the node index of the centroid
-        newElem(j,:)=[offset+i,...
-                        elemSplit(i,faceorder(j,1)),...
-                        elemSplit(i,faceorder(j,2)),...
-                        elemSplit(i,faceorder(j,3))];
-        newLocalEdge(j,:)=labelSingleElem(newElem(j,:),edgeSet);
-        if sum(logical(newLocalEdge(j,:)))>2
-           warning('Split failed')
+i=1;
+while i<=size(elem,1)
+    enum = 0; % number of edges being labeled
+    for j=1:6
+        edge=sort([elem(i,eo(j,1)) elem(i,eo(j,2))]);
+        if e2r.containsKey(num2str(edge))
+            eroi(i,j)=e2r.get(num2str(edge));
+            enum=enum+1;
         end
     end
-    splitElem{i}=newElem;
-    splitLocalEdge{i}=newLocalEdge;
-end
-
-%% Insert new elements to original elements
-
-if indexForSplit(1)==1
-    elemInserted=[];
-    edgeIndexInserted=[];
-else
-    elemInserted=elem(1:indexForSplit(1)-1,:);
-    edgeIndexInserted=edgeIndex(1:indexForSplit(1)-1,:);
-end
-elemInserted=[elemInserted; splitElem{1}];
-edgeIndexInserted=[edgeIndexInserted; splitLocalEdge{1}];
-
-for i=2:length(indexForSplit)
-    elemInserted=[elemInserted; elem(indexForSplit(i-1)+1:indexForSplit(i)-1,:)];
-    elemInserted=[elemInserted; splitElem{i}];
     
-    edgeIndexInserted=[edgeIndexInserted; edgeIndex(indexForSplit(i-1)+1:indexForSplit(i)-1,:)];
-    edgeIndexInserted=[edgeIndexInserted; splitLocalEdge{i}];
-end
-
-if indexForSplit(end)<size(elem,1)
-    elemInserted=[elemInserted; elem(indexForSplit(end)+1:end,:)];
-    edgeIndexInserted=[edgeIndexInserted; edgeIndex(indexForSplit(end)+1:end,:)];
-end
-fprintf('Finished relabeling.\n')
-
-%% function: label local edges for single element
-function localEdge=labelSingleElem(elem,edgeSet)
-% label a single element
-n2e=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
-localEdge=zeros(1,size(n2e,1));
-c=1;
-for i=1:size(n2e,1)
-    % check if the edge is in the set
-    curEdge=sort([elem(n2e(i,1)) elem(n2e(i,2))],2);
-    if edgeSet.contains(num2str(curEdge))
-        localEdge(c)=i;
-        c=c+1;
-    end
-end
-
-
-%% function: obtain the radii for the labeled edges
-function radii=getVesselRadii(elem,index,noder)
-n2e=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4];  % node to edge mapping
-radii=zeros(size(index,1),2);
-for i=1:size(index,1)
-    % edge 1
-    if index(i,1)~=0
-        ei1=n2e(index(i,1),:);
-        radii(i,1)=max([noder(elem(i,ei1(1))),noder(elem(i,ei1(2)))]);
+    if enum>2
+        % if there are more than 2 labeled edges,split and relabel the 
+        % current element
+        [node,elem,eroi]=splitelem(node,elem,eroi,e2r,i);
+        i=i+3; % replace the old element with four new elements
     end
     
-    % edge 2
-    if index(i,2)~=0
-        ei2=n2e(index(i,2),:);
-        radii(i,2)=max([noder(elem(i,ei2(1))),noder(elem(i,ei2(2)))]);
-    end
+    i=i+1;
 end
 
+%% function
+function [node,elem,eroi]=splitelem(node,elem,eroi,e2r,index)
+% 
+% split the element that has over 2 labeled edges
+% 
+% input:
+%     node:  node coordinates for elem
+%     elem:  input mesh containing the edges
+%     eroi:  input data for cfg.edgeroi
+%     e2r:   java.util.HashMap mapping edge to its radius
+%     index: index of element that needs to be split
+% 
+% output:
+%     elem:  mesh after spliting
 
-%% function: build cfg for immc
-function cfg=inputMesh2Cfg(elem,index,radii,node,noder)
+eo=[1 2; 1 3; 1 4; 2 3; 2 4; 3 4]; % edge order in an element
+fo=[1 2 3; 1 2 4; 1 3 4; 2 3 4]; % face order in an element
 
-% turn on iMMC
-cfg.implicit=1;
+selem=elem(index,:); % element to be split
+c=meshcentroid(node,selem); % centroid of the element to be split
+cindex=size(node,1)+1;
+node=[node; c]; % put the centroid in the end
 
-% post-processing to fit to iMMC input format
-index=index(:,1:4);
-index(index==0)=7;
-% 0 to 5 represet local edge labeled as vessel, 6 represent not labeled as
-% vessel
-index=index-1;
+newelem=[]; % 4 elements that have been split using centroid
+newroi=[];  % new edge roi
+for i=1:size(fo,1)
+    e=[selem(fo(i,1)) selem(fo(i,2)) selem(fo(i,3)) cindex];
+    newelem=[newelem; e];
+    
+    r=zeros(1,size(eo,1));
+    % check the edge that has radius and fill it in edgeroi
+    for j=1:size(eo,1)
+        edge=sort([e(eo(j,1)),e(eo(j,2))]);
+        if e2r.containsKey(num2str(edge))
+            r(j)=e2r.get(num2str(edge));
+        end
+    end
+    newroi=[newroi; r];
+end
 
-node=[node noder];
-elem=[elem index radii zeros(size(radii,1),2)];
-
-cfg.node=node;
-cfg.elem=elem;
+% remove the old element and insert the new elements
+elem=[elem(1:index-1,:); newelem; elem(index+1:end,:)];
+% remove the old roi and insert the new rois
+eroi=[eroi(1:index-1,:); newroi; eroi(index+1:end,:)];
