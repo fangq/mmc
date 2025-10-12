c1136255e543c48194c2b80d3be263b8c116f866
Qianqian Fang <fangqq@gmail.com>
2025-10-12 00:12:27 -0400

    [deploy] fix octave 5.2 opencl not linked bug, fix command

--- commons/Makefile_common.mk
+++ commons/Makefile_common.mk
@@ -223,7 +223,7 @@ OCT_LDFLAGS := $(shell mkoctfile -p LDFLAGS)
 
 oct:            BINARY=mmc.mex
 oct octomp:     ARFLAGS+=--mex -DMATLAB_MEX_FILE mmclab.cpp -I$(INCLUDEDIR)
-oct octomp:     LFLAGS='$(LIBOPENCL)' AR=CC=$(CC) CXX=$(CXX) LDFLAGS='$(OCT_LDFLAGS) $(OPENMPLIB) $(LIBOPENCL) $(MEXLINKOPT)' CPPFLAGS='$(CCFLAGS) $(USERCCFLAGS) -std=c++11' $(USEROCTOPT) $(MKOCT)
+oct octomp:     AR=CC=$(CC) CXX=$(CXX) LFLAGS='$(LIBOPENCL)' LDFLAGS='$(OCT_LDFLAGS) $(OPENMPLIB) $(LIBOPENCL) $(MEXLINKOPT)' CPPFLAGS='$(CCFLAGS) $(USERCCFLAGS) -std=c++11' $(USEROCTOPT) $(MKOCT)
 oct octomp:     USERARFLAGS+=-o $(BINDIR)/mmc
 
 debug:     sse
