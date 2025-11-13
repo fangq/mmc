5e5cb81ec54b8064a998641707f5b1a149dd3cbf
Qianqian Fang <fangqq@gmail.com>
2025-11-13 17:31:28 -0500

    [pmmc] bump pmmc version to 0.3.7 after fix #108

--- pmmc/setup.py
+++ pmmc/setup.py
@@ -127,7 +127,7 @@ class CMakeBuild(build_ext):
 setup(
     name="pmmc",
     packages=["pmmc"],
-    version="0.3.6",
+    version="0.3.7",
     requires=["numpy"],
     license="GPLv3+",
     author="Qianqian Fang",
